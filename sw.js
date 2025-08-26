a0945b5414a3ff853961255f587ed7a6c44b00f3
trifancristi19 <cristian.trifan@student.nhlstenden.com>
2025-08-26 20:35:20 +0200

    Add performance optimizations: service worker and server config

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'portfolio-v1.0';
+const urlsToCache = [
+  '/',
+  '/css/style.css',
+  '/js/script.js',
+  '/assets/images/Poza%20Profil.jpg',
+  '/assets/images/hacker.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Share+Tech+Mono&family=Source+Code+Pro:wght@300;400;500;700&display=swap'
+];
+
+// Install event - cache resources
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event - serve from cache when possible
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
